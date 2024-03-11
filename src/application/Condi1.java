dc46cc0d92142fa90fa1f723e11c5d7d610639a3
Paulo Felipe Sardeiro <131005207+SardeiroPaulo@users.noreply.github.com>
2024-03-11 10:37:43 -0300

    +1

---
+++ src/application/Condi1.java
@@ -0,0 +1,18 @@
+// número par ou impar com if/else
+
+package application;
+
+public class Condi1 {
+    public static void main(String[] args) {
+
+        int n = 11;
+
+        if ( n % 2 ==0 ){
+            System.out.println("Número par");
+        }
+        else if ( n % 2 != 0 ){
+            System.out.println("Número ímpar");
+        }
+
+    }
+}
